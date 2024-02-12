2b912a5d9858fbd8e59696b046a9426163047859
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2024-02-12 17:44:26 +0500

    zb

--- bot/handlers/get_api_contents.py
+++ bot/handlers/get_api_contents.py
@@ -5,12 +5,14 @@ import logging
 # from bot.config import BASE_API
 
 
-def get_external_data():
+def get_group_data():
     try:
-        response = requests.get("http://127.0.0.1:8000/api")
+        response = requests.get("http://127.0.0.1:8000/api/groups/")
         response.raise_for_status()
         data = response.json()
-        return data
+
+        groups = data.get("title")
+        return groups
     except Exception as e:
         logging.error(f"Error fetching external data: {str(e)}")
         return None
--- bot/handlers/personal_actions.py
+++ bot/handlers/personal_actions.py
@@ -1,6 +1,6 @@
 
 from aiogram import types
-from bot.handlers.get_api_contents import get_external_data
+from bot.handlers.get_api_contents import get_group_data
 
 from dispatcher import dp, bot
 
@@ -13,7 +13,7 @@ async def cmd_ping_bot(message: types.Message):
 
 @dp.message_handler(commands=['getdata'])
 async def send_data(message: types.Message):
-    external_data = get_external_data()
+    external_data = get_group_data()
     if external_data:
         await message.answer(f"Data from the external API: {external_data}")
     else:
